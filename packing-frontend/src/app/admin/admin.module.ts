207a549afa5426bd8beec24201d7139edc37ba40
solozo <ozoemenamsolomon@yahoo.com>
2023-05-21 00:11:41 +0200

    import Sidenav module

--- packing-frontend/src/app/admin/admin.module.ts
+++ packing-frontend/src/app/admin/admin.module.ts
@@ -2,17 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule, RoutedComponent } from './admin-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-
-
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  declarations: [
-    RoutedComponent,
-    DashboardComponent
-  ],
-  imports: [
-    CommonModule,
-    AdminRoutingModule
-  ]
+  declarations: [RoutedComponent, DashboardComponent],
+  imports: [CommonModule, AdminRoutingModule, MatSidenavModule],
 })
 export class AdminModule { }
