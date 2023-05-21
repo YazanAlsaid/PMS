67f297f64ff7e97ecb9a929503b18af5f8b614e9
Ozoemenam Solomon Obinna <solomon.ozoemenam@stud.fra-uas.de>
2023-05-21 18:26:50 +0200

    refactor dashboard data

--- packing-frontend/src/app/admin/components/dashboard/dashboard.component.ts
+++ packing-frontend/src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,30 +1,40 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Data, Router } from '@angular/router';
 
 export interface Row {
-  name: string;
   id: number;
   weight: number;
 }
 
 const ELEMENT_DATA: Row[] = [
-  { id: 1, name: 'Hydrogen', weight: 1.0079 },
-  { id: 2, name: 'Helium', weight: 4.0026 },
-  { id: 3, name: 'Lithium', weight: 6.941 },
-  { id: 4, name: 'Beryllium', weight: 9.0122 },
-  { id: 5, name: 'Boron', weight: 10.811 },
-  { id: 6, name: 'Carbon', weight: 12.0107 },
-  { id: 7, name: 'Nitrogen', weight: 14.0067 },
-  { id: 8, name: 'Oxygen', weight: 15.9994 },
-  { id: 9, name: 'Fluorine', weight: 18.9984 },
-  { id: 10, name: 'Neon', weight: 20.1797 },
+  { id: 1, weight: 1.0079 },
+  { id: 2, weight: 4.0026 },
+  { id: 3, weight: 6.941 },
+  { id: 4, weight: 9.0122 },
+  { id: 5, weight: 10.811 },
+  { id: 6, weight: 12.0107 },
+  { id: 7, weight: 14.0067 },
+  { id: 8, weight: 15.9994 },
+  { id: 9, weight: 18.9984 },
+  { id: 10, weight: 20.1797 },
 ];
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  sub: any;
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
   displayedColumns: string[] = ['id', 'name', 'weight', 'action'];
   dataSource = ELEMENT_DATA;
+  data: Data | undefined;
+
+  ngOnInit() {
+    this.sub = this.activatedRoute.data.subscribe((v) => (this.data = v));
+  }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
