c4d3c7b7a60d771f16fb6c0266655e1d72bdcae3
Ozoemenam Solomon Obinna <solomon.ozoemenam@stud.fra-uas.de>
2023-05-22 13:01:59 +0200

    implement start NFC scan

--- packing-frontend/src/app/scanner/components/entry/entry.component.ts
+++ packing-frontend/src/app/scanner/components/entry/entry.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
-  styleUrls: ['./entry.component.scss']
+  styleUrls: ['./entry.component.scss'],
 })
 export class EntryComponent {
+  checkNFCSupport() {
+    if (!('NDEFReader' in window)) {
+      return console.log('Web NFC not supported');
+    }
+  }
 
+  async startScan() {
+    this.checkNFCSupport();
+
+    try {
+      const ndef = new NDEFReader();
+      await ndef.scan();
+
+      ndef.addEventListener('readingerror', () => {
+        console.log("Couldn't read from NFC tag, try another one");
+      });
+
+      ndef.addEventListener('reading', (e) => {
+        console.log({ e });
+      });
+    } catch (error) {
+      console.log('Error occured!');
+      console.log({ error });
+    }
+  }
 }
