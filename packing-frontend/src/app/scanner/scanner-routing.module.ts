25792b314bfddb85ec4ebfa236709040bfc65da0
Ozoemenam Solomon Obinna <solomon.ozoemenam@stud.fra-uas.de>
2023-05-22 13:26:49 +0200

    entry scanner UI

--- packing-frontend/src/app/scanner/scanner-routing.module.ts
+++ packing-frontend/src/app/scanner/scanner-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntryComponent } from './components/entry/entry.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'entry', component: EntryComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ScannerRoutingModule { }
+export class ScannerRoutingModule {}
