ce92b385574057ed18363603f933c07755f7c24d
Ebrahim <bronum89@gmail.com>
2023-05-24 16:48:55 +0200

    Updated UserRepository

--- parking-backend/src/main/java/edu/fra/uas/parking/repository/UserRepository.java
+++ parking-backend/src/main/java/edu/fra/uas/parking/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package edu.fra.uas.parking.repository;
 
-public interface UserRepository extends org.springframework.data.jpa.repository.JpaRepository<edu.fra.uas.parking.entity.User, java.lang.Long> {
+import edu.fra.uas.parking.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
