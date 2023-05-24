e3c30955c775655ddcc210210ae164521b46e662
Ebrahim <bronum89@gmail.com>
2023-05-24 16:45:45 +0200

    I created  Repository Package and UserRepository

---
+++ parking-backend/src/main/java/edu/fra/uas/parking/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package edu.fra.uas.parking.repository;
+
+public interface UserRepository extends org.springframework.data.jpa.repository.JpaRepository<edu.fra.uas.parking.entity.User, java.lang.Long> {
+}
