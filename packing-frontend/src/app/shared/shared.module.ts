b08679119a14df88afdce422545045a4cefdf76f
Kaddour Alnaasan <kaddour@alnaasan>
2023-05-27 08:00:37 +0200

    create Sidebar in shared and using it in AppDashboard

--- packing-frontend/src/app/shared/shared.module.ts
+++ packing-frontend/src/app/shared/shared.module.ts
@@ -6,19 +6,26 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterLink, RouterOutlet } from "@angular/router";
 
 @NgModule({
   declarations: [
-    NavComponent
+    NavComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatSidenavModule,
+    RouterLink,
+    RouterOutlet
   ], exports: [
-    NavComponent
+    NavComponent,
+    SidebarComponent,
   ]
 })
 export class SharedModule { }
