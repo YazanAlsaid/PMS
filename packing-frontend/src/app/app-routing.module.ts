907017380186aafcf0a575a57087a7410c1861c6
Ozoemenam Solomon Obinna <solomon.ozoemenam@stud.fra-uas.de>
2023-05-28 14:47:42 +0200

    reinsert scanner route

--- packing-frontend/src/app/app-routing.module.ts
+++ packing-frontend/src/app/app-routing.module.ts
@@ -2,16 +2,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path: 'auth', loadChildren: () => import(`./auth/auth.module`).then(m => m.AuthModule) },
-    { path: 'dashboard', loadChildren: () => import(`./admin/admin.module`).then(m => m.AdminModule) },
-    { path: 'home', loadChildren: () => import(`./user/user.module`).then(m => m.UserModule) },
-    { path: '', redirectTo: 'auth', pathMatch: 'full' },
-    { path: '**', redirectTo: 'auth', pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadChildren: () => import(`./auth/auth.module`).then((m) => m.AuthModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import(`./admin/admin.module`).then((m) => m.AdminModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () => import(`./user/user.module`).then((m) => m.UserModule),
+  },
+  {
+    path: 'scanner',
+    loadChildren: () =>
+      import(`./scanner/scanner.module`).then((m) => m.ScannerModule),
+  },
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  { path: '**', redirectTo: 'auth', pathMatch: 'full' },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
