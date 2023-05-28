88eb16002b11dc61ae687e76b57e81778e0180bd
Ebrahim <bronum89@gmail.com>
2023-05-28 15:03:03 +0200

    I fixed and added new relations with The Privilege class.

--- parking-backend/src/main/java/edu/fra/uas/parking/entity/Reservation.java
+++ parking-backend/src/main/java/edu/fra/uas/parking/entity/Reservation.java
@@ -1,8 +1,6 @@
 package edu.fra.uas.parking.entity;
 
-import com.fasterxml.jackson.databind.ser.Serializers;
-import jakarta.persistence.*;
-
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
