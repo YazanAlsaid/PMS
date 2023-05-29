d156c17ae53ff96aa4982fd3bc487677879fef8a
Mohammed Dawoud <damohammed58@gmail.com>
2023-05-29 14:55:35 +0200

    delete TimeStampDetailsController.java and TimeStampDetailsRepository

--- parking-backend/src/main/java/edu/fra/uas/parking/repository/TimeStampDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.fra.uas.parking.repository;
-
-import edu.fra.uas.parking.entity.TimeStampDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TimeStampDetailsRepository extends JpaRepository<TimeStampDetails, Long> {
-}
