22fe0f446f5dfe65d709423aae20de4113019354
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2023-05-31 09:27:55 +0200

    add dashboard and floors routes

--- packing-frontend/src/app/user/user-routing.module.ts
+++ packing-frontend/src/app/user/user-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserAppComponent } from './components/user-app/user-app.component';
 import {BuildingComponent} from "./components/building/building.component";
 import {SlotComponent} from "./components/slot/slot.component";
+import {UserDashboardComponent} from "./components/user-dashboard/user-dashboard.component";
+import {FloorsComponent} from "./components/floors/floors.component";
 
 const routes: Routes = [
   { path: '', component: UserAppComponent, children:[
+      {path:'dashboard',component:UserDashboardComponent,pathMatch: 'full'},
       {path:'buildings',component:BuildingComponent,pathMatch: 'full'},
+      {path:'floors',component:FloorsComponent,pathMatch: 'full'},
       {path:'slots',component:SlotComponent,pathMatch: 'full'},
     ]
   },
@@ -22,5 +26,7 @@ export class UserRoutingModule { }
 export const RoutedComponent = [
     UserAppComponent,
     BuildingComponent,
+    FloorsComponent,
     SlotComponent,
+    UserDashboardComponent
 ];
