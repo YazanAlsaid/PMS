8c1455893c2d18518d5018339090d1211689c985
Ebrahim <bronum89@gmail.com>
2023-06-01 16:37:29 +0200

    create add user dialog

---
+++ packing-frontend/src/app/admin/components/add-user-dialog/add-user-dialog.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-add-user-dialog',
+  templateUrl: './add-user-dialog.component.html',
+  styleUrls: ['./add-user-dialog.component.scss']
+})
+export class AddUserDialogComponent {
+
+  dialogForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    public dialogRef: MatDialogRef<AddUserDialogComponent>
+  ) {
+    this.dialogForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.dialogForm.valid) {
+      // Hier kannst du den Code ausführen, um die eingegebenen Daten zu verarbeiten
+      console.log(this.dialogForm.value);
+
+      // Schließe den Dialog
+      this.dialogRef.close();
+    }
+  }
+}
