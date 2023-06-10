1bc3b8397edf1bc9260878a1d8185aa4a8ab4a0a
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2023-06-10 13:41:20 +0200

    fix import

--- parking-backend/src/main/java/edu/fra/uas/parking/repository/FloorRepository.java
+++ parking-backend/src/main/java/edu/fra/uas/parking/repository/FloorRepository.java
@@ -1,6 +1,5 @@
 package edu.fra.uas.parking.repository;
 
-import edu.fra.uas.parking.entity.Building;
 import edu.fra.uas.parking.entity.Floor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
