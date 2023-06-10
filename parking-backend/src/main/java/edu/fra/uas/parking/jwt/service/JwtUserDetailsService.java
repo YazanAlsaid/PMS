4f12e6391665a0dbfdb0726b2273c0b73cf6afb6
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2023-06-10 14:02:44 +0200

    add jwt

---
+++ parking-backend/src/main/java/edu/fra/uas/parking/jwt/service/JwtUserDetailsService.java
@@ -0,0 +1,33 @@
+package edu.fra.uas.parking.jwt.service;
+
+import edu.fra.uas.parking.entity.User;
+import edu.fra.uas.parking.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public JwtUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> optionalUser = this.userRepository.findByEmail(email);
+        if (optionalUser.isPresent()) {
+            return new org.springframework.security.core.userdetails.User(optionalUser.get().getEmail(), optionalUser.get().getPassword(), new ArrayList<>());
+        }
+
+        throw new UsernameNotFoundException("User can not found by email: " + email);
+    }
+}
