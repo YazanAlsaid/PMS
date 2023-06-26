1c6586ece0108de31d749a28518ee60e1cdf8894
Ebrahim <bronum89@gmail.com>
2023-06-26 10:03:39 +0200

    deleted trash

--- parking-backend/src/main/java/edu/fra/uas/parking/entity/Address.java
+++ parking-backend/src/main/java/edu/fra/uas/parking/entity/Address.java
@@ -2,12 +2,7 @@ package edu.fra.uas.parking.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Size;
 import java.util.Objects;
 
