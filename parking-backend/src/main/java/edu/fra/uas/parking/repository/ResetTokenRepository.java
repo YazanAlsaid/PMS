3b54de3a1b8b1d1a6b276ef87d87722f31e216f4
Ebrahim <bronum89@gmail.com>
2023-06-29 20:25:58 +0200

    created the ResetTokenRepository

--- parking-backend/src/main/java/edu/fra/uas/parking/repository/ResetTokenRepository.java
+++ parking-backend/src/main/java/edu/fra/uas/parking/repository/ResetTokenRepository.java
@@ -4,6 +4,10 @@ import edu.fra.uas.parking.entity.ResetToken;
 import edu.fra.uas.parking.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ResetTokenRepository extends JpaRepository<ResetToken, Long> {
     ResetToken findByUser(User user);
+
+    ResetToken findByToken(String token);
 }
