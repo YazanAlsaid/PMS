6a6637a4d7036de58f70789b688726021a8fba72
Ebrahim <bronum89@gmail.com>
2023-06-29 20:23:39 +0200

    added the ResetPasswordController

--- parking-backend/src/main/java/edu/fra/uas/parking/jwt/controller/ResetPasswordController.java
+++ parking-backend/src/main/java/edu/fra/uas/parking/jwt/controller/ResetPasswordController.java
@@ -2,11 +2,15 @@ package edu.fra.uas.parking.jwt.controller;
 
 // Java Spring Boot Controller (ResetPasswordController)
 
+import edu.fra.uas.parking.common.ResponseMessage;
+import edu.fra.uas.parking.entity.ChangePassword;
 import edu.fra.uas.parking.entity.ResetToken;
 import edu.fra.uas.parking.entity.User;
 import edu.fra.uas.parking.repository.ResetTokenRepository;
 import edu.fra.uas.parking.repository.UserRepository;
 import edu.fra.uas.parking.service.EmailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +27,7 @@ import java.util.UUID;
 @RequestMapping("/reset")
 public class ResetPasswordController {
 
+    private final Logger logger = LoggerFactory.getLogger(ResetPasswordController.class);
     private final UserRepository userRepository;
     private final EmailService emailService;
     private final ResetTokenRepository resetTokenRepository;
@@ -37,14 +42,15 @@ public class ResetPasswordController {
         this.request = request;
     }
 
-    @PostMapping("/reset-password")
-    public ResponseEntity<String> resetPassword(@RequestBody String email) {
-        Optional<User> user = userRepository.findByEmail(email);
 
+    @PostMapping("/reset-password")
+    public ResponseEntity<ResponseMessage> resetPassword(@RequestBody String email) {
+        logger.debug("POST /reset/reset-password");
+        Optional<User> user = userRepository.findByEmail(email);
         if (user.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body("User with the provided email does not exist");
+            return new ResponseEntity<>(new ResponseMessage("User with the provided email does not exist", null), HttpStatus.NOT_FOUND);
         }
+
 
         ResetToken existingToken = resetTokenRepository.findByUser(user.get());
         if (existingToken != null) {
@@ -59,19 +65,64 @@ public class ResetPasswordController {
         resetTokenRepository.save(resetTokenObj);
 
         sendResetTokenViaEmail(user.get().getEmail(), resetToken);
-        return ResponseEntity.ok("Password reset request submitted successfully");
+        return new ResponseEntity<>(new ResponseMessage("Password reset request submitted successfully", null), HttpStatus.OK);
     }
 
-    private void sendResetTokenViaEmail(String email, String resetToken) {
-        String host;
-        try {
-            URI uri = new URI(request.getRequestURL().toString());
-            host = uri.getScheme() + "://" + uri.getAuthority();
-        } catch (URISyntaxException e) {
-            host = "http://localhost:8080"; // Fallback if URI syntax is invalid
+    @PostMapping("/change-password")
+    public ResponseEntity<ResponseMessage> changePasswordPerToken(@RequestBody ChangePassword changePassword) {
+        String token = changePassword.getToken();
+        String newPassword = changePassword.getPassword();
+        String confirmPassword = changePassword.getConfirmPassword();
+
+        if (token == null || token.isEmpty()) {
+            return new ResponseEntity<>(new ResponseMessage("Token cannot be empty", null), HttpStatus.BAD_REQUEST);
         }
 
-        String resetUrl = host + "/auth/reset-password?token=" + resetToken;
+        if (newPassword == null || newPassword.isEmpty()) {
+            return new ResponseEntity<>(new ResponseMessage("New password cannot be empty", null), HttpStatus.BAD_REQUEST);
+        }
+
+        if (confirmPassword == null || confirmPassword.isEmpty()) {
+            return new ResponseEntity<>(new ResponseMessage("Confirm password cannot be empty", null), HttpStatus.BAD_REQUEST);
+        }
+
+        if (!newPassword.equals(confirmPassword)) {
+            return new ResponseEntity<>(new ResponseMessage("New password and confirm password do not match", null), HttpStatus.BAD_REQUEST);
+        }
+
+        ResetToken resetToken = resetTokenRepository.findByToken(token);
+        if (resetToken == null) {
+            return new ResponseEntity<>(new ResponseMessage("Invalid token", null), HttpStatus.BAD_REQUEST);
+        }
+
+        if (resetToken.getExpirationDateTime().isBefore(LocalDateTime.now())) {
+            return new ResponseEntity<>(new ResponseMessage("Token has expired", null), HttpStatus.BAD_REQUEST);
+        }
+
+        User user = resetToken.getUser();
+        user.setHashedPassword(newPassword);
+        this.userRepository.save(user);
+        this.resetTokenRepository.delete(resetToken);
+
+        // Invalidate the token by updating its status
+        this.resetTokenRepository.delete(resetToken);
+
+        return new ResponseEntity<>(new ResponseMessage("Password changed successfully", null), HttpStatus.ACCEPTED);
+    }
+    private void sendResetTokenViaEmail(String email, String resetToken) {
+        String host;
+        if (request.getRequestURL().toString().contains("localhost")) {
+            host = "http://localhost:4200"; // Application running locally
+        } else {
+            try {
+                URI uri = new URI(request.getRequestURL().toString());
+                host = uri.getScheme() + "://" + uri.getAuthority(); // Domain in production
+            } catch (URISyntaxException e) {
+                host = "http://localhost:4200"; // Fallback to localhost if URI syntax is invalid
+            }
+        }
+
+        String resetUrl = host + "/auth/change-password?token=" + resetToken;
 
         String subject = "Password Reset";
         String text = "To reset your password, click on the following link: " + resetUrl;
@@ -79,26 +130,5 @@ public class ResetPasswordController {
         emailService.sendEmail(email, subject, text);
     }
 
-    @GetMapping("/check-email-exists")
-    public ResponseEntity<EmailExistsResponse> checkEmailExists(@RequestParam String email) {
-        Optional<User> user = userRepository.findByEmail(email);
 
-        if (user.isPresent()) {
-            return ResponseEntity.ok(new EmailExistsResponse(true));
-        } else {
-            return ResponseEntity.ok(new EmailExistsResponse(false));
-        }
-    }
-
-    private static class EmailExistsResponse {
-        private final boolean exists;
-
-        public EmailExistsResponse(boolean exists) {
-            this.exists = exists;
-        }
-
-        public boolean isExists() {
-            return exists;
-        }
-    }
 }
