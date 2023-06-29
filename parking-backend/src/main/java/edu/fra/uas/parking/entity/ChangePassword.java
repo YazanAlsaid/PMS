0c5c11bf32eb72beeb7bc25f06d44674affc6a9d
Ebrahim <bronum89@gmail.com>
2023-06-29 20:17:53 +0200

    added ChangePassword class

--- parking-backend/src/main/java/edu/fra/uas/parking/entity/ChangePassword.java
+++ parking-backend/src/main/java/edu/fra/uas/parking/entity/ChangePassword.java
@@ -1,2 +1,20 @@
-package edu.fra.uas.parking.entity;public class ChnagePassword {
+package edu.fra.uas.parking.entity;
+
+public class ChangePassword {
+
+    private String token;
+    private String password;
+    private String confirmPassword;
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getConfirmPassword() {
+            return confirmPassword;
+    }
 }
