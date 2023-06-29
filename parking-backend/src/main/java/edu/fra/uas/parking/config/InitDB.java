33aad38a53786ace678736bdf8d293c2c431b99c
Ozoemenam Solomon Obinna <solomon.ozoemenam@stud.fra-uas.de>
2023-06-29 09:40:20 +0000

    ^

--- parking-backend/src/main/java/edu/fra/uas/parking/config/InitDB.java
+++ parking-backend/src/main/java/edu/fra/uas/parking/config/InitDB.java
@@ -21,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Optional;
