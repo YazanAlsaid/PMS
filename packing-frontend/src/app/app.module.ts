d5522f85f8b8929c21fcca795d376a6e8eed42a5
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2023-07-03 19:26:23 +0200

    create SnackPopup

--- packing-frontend/src/app/app.module.ts
+++ packing-frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SharedModule } from './shared/shared.module';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { TokenInterceptorInterceptor } from "./shared/services/token-interceptor.interceptor";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +18,7 @@ import { TokenInterceptorInterceptor } from "./shared/services/token-interceptor
     HttpClientModule,
     BrowserAnimationsModule,
     SharedModule,
+    MatSnackBarModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory,
