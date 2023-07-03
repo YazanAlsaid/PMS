71edd98c10df403da193b7f1052eb1f01b484cf9
Ebrahim <bronum89@gmail.com>
2023-07-03 18:54:21 +0200

    updated the class

--- packing-frontend/src/app/shared/model/nfc.ts
+++ packing-frontend/src/app/shared/model/nfc.ts
@@ -2,22 +2,19 @@ import {Reservation} from "./reservation";
 import {User} from "./user";
 
 export class Nfc {
-  public id: number;
-  public name: string;
-  public nfcFrom: Date;
-  public nfcTo: Date;
-  public reservations: Reservation[];
-  public user!: User;
-  public createdAt: Date;
-  public updatedAt: Date;
+  public id !: number;
+  public serialNumber : string;
+  public nfcFrom : Date;
+  public nfcTo : Date;
+  public reservations !: Reservation[];
+  public user: User;
+  public createdAt!: Date;
+  public updatedAt!: Date;
 
-  constructor(id: number, name: string, nfcFrom: Date, nfcTo: Date, reservations: Reservation[], createdAt: Date, updatedAt: Date) {
-    this.id = id;
-    this.name = name;
+  constructor(serialNumber: string, nfcFrom: Date, nfcTo: Date, user: User) {
+    this.serialNumber = serialNumber;
     this.nfcFrom = nfcFrom;
     this.nfcTo = nfcTo;
-    this.reservations = reservations;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
+    this.user = user;
   }
 }
