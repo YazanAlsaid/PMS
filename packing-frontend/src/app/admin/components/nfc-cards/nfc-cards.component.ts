29d9add69f72316b5e6b5ad3d328915a2e0ae6f7
Ebrahim <bronum89@gmail.com>
2023-07-03 20:08:00 +0200

    Snack Bar

--- packing-frontend/src/app/admin/components/nfc-cards/nfc-cards.component.ts
+++ packing-frontend/src/app/admin/components/nfc-cards/nfc-cards.component.ts
@@ -3,9 +3,9 @@ import {MatPaginator} from "@angular/material/paginator";
 import {MatTableDataSource} from "@angular/material/table";
 import {ClientNfcService} from "../../../shared/services/client-nfc.service";
 import {ActivatedRoute} from "@angular/router";
-import {AddParkDialogComponent} from "../add-park-dialog/add-park-dialog.component";
 import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
 import { AddNfcDialogComponent } from '../add-nfc-dialog/add-nfc-dialog.component';
+import { SnackPopupService } from 'src/app/shared/services/snack-popup.service';
 
 @Component({
   selector: 'app-nfc-cards',
@@ -20,7 +20,8 @@ export class NfcCardsComponent implements AfterViewInit, OnInit {
 
   constructor(private clientNfc: ClientNfcService,
               private activatedRoute: ActivatedRoute,
-              private dialog: MatDialog) {
+              private dialog: MatDialog,
+              private sanckPopup: SnackPopupService) {
   }
 
   private dialogConfig: MatDialogConfig = {
@@ -69,6 +70,7 @@ export class NfcCardsComponent implements AfterViewInit, OnInit {
               this.dataSource.data = this.dataSource.data.slice();
               // Reset the paginator to the first page
               this.paginator.firstPage();
+              this.sanckPopup.open(res.message);
             },
             (err: any) => console.log(err.error.error)
           )
